2cb35661a4660a6373d69d9c06eb9be4279bfbb7
Robert Krawczyk <robert@wearemotto.com>
2025-05-08 12:35:29 +0200

    feat: Action Group and buttons

--- parts/icons/arrow-right.php
+++ parts/icons/arrow-right.php
@@ -1,3 +1,3 @@
-<svg class="<?php echo isset($args['classes']) ? $args['classes'] : ''; ?>" width="22" height="22" viewBox="0 0 22 22" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4.58594 11.0007H17.4193M17.4193 11.0007L13.7526 14.6673M17.4193 11.0007L13.7526 7.33398" stroke-width="1.5" stroke-linecap="round" />
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path d="M4.16406 10H15.8307M15.8307 10L10.8307 15M15.8307 10L10.8307 5" stroke="#F6F5FA" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
