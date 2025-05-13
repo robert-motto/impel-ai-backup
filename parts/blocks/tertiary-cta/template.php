cbc049e331866f69a84b8a0981ec253b407d7518
Robert Krawczyk <robert@wearemotto.com>
2025-05-13 14:05:04 +0200

    feat: Tertiary CTA

--- parts/blocks/tertiary-cta/template.php
+++ parts/blocks/tertiary-cta/template.php
@@ -7,115 +7,49 @@
 	<?php block_preview(__FILE__); ?>
 <?php else :?>
 	<?php
-		$group            = isset($args['data']) ? $args['data'] : blockFieldGroup(__FILE__);
-		$caption          = $group['caption'] ?? '';
-		$heading          = $group['heading'] ?? '';
-		$content          = $group['content'] ?? '';
-		$media_type       = $group['media_type'] ?? 'none';
-		$image            = $group['image'] ?? '';
-		$has_background   = $group['has_background'] ?? true;
-		$background_color = $group['background_color'] ?? 'light';
-		$has_border_radius = $group['has_border_radius'] ?? true;
-		$buttons_group = $group['buttons_group'] ?? [];
+		$group			= isset($args['data']) ? $args['data'] : blockFieldGroup(__FILE__);
+		$color_mode	= $group['section_settings_group']['mode'] ?? 'light';
+		$caption		= $group['caption'] ?? '';
+		$heading		= $group['heading'] ?? '';
+		$content		= $group['content'] ?? '';
+		$buttons		= $group['action_group_group'] ?? [];
 
-		// Set fallback image if none is selected and media type is image
-		if ($media_type === 'image' && (empty($image) || !isset($image['id']))) {
-			$fallback_image_path = get_template_directory() . '/screenshot.jpg';
-			$fallback_image_uri = get_template_directory_uri() . '/screenshot.jpg';
-			if (file_exists($fallback_image_path)) {
-				$fallback_attachment = attachment_url_to_postid($fallback_image_uri);
-				if ($fallback_attachment) {
-					$image = ['id' => $fallback_attachment];
-				} else {
-					$image = [
-						'url' => $fallback_image_uri,
-						'alt' => wp_strip_all_tags($heading ?? 'Tertiary CTA image')
-					];
-				}
-			}
-		}
-
-		$classes = ['l-section', 'l-section--tertiary-cta', 'js-tertiary-cta'];
-		$block_classes = ['tertiary-cta'];
-
-		if ($has_background) {
-			$block_classes[] = 'tertiary-cta--has-background';
-			$block_classes[] = 'tertiary-cta--bg-' . $background_color;
-
-			if ($background_color === 'dark') {
-				$block_classes[] = 'is-dark';
-			}
-		}
-
-		if ($has_border_radius) {
-			$block_classes[] = 'tertiary-cta--has-radius';
-		}
-
-		if ($media_type === 'image') {
-			$block_classes[] = 'tertiary-cta--has-image';
-		}
+		// Section classes
+		$classes = ['l-section', 'l-section--tertiary-cta', 'color-mode-' . ($color_mode === 'is-dark' ? 'is-light' : 'is-dark')];
 	?>
 	<section <?php echo section_settings_id($group); ?> class="<?php echo esc_attr(implode(' ', $classes)); ?> <?php echo section_settings_padding_classes($group); ?>" data-block="tertiary-cta">
 		<div class="l-wrapper">
-			<div class="<?php echo esc_attr(implode(' ', $block_classes)); ?>">
+			<div class="tertiary-cta__box" style="background-image: url('<?php echo esc_url( get_template_directory_uri() . '/parts/blocks/tertiary-cta/cta_tertiary_' . ( $color_mode === 'is-dark' ? 'light' : 'dark' ) . '_glimmer.jpg' ); ?>');">
 				<div class="tertiary-cta__content">
 					<?php if (!empty($caption)) : ?>
-						<div class="tertiary-cta__caption">
+						<p class="tertiary-cta__caption">
 							<?php echo esc_html($caption); ?>
-						</div>
+						</p>
 					<?php endif; ?>
 
 					<?php if (!empty($heading)) : ?>
-						<h2 class="tertiary-cta__heading">
+						<div class="tertiary-cta__heading">
 							<?php echo $heading; ?>
-						</h2>
-					<?php endif; ?>
-
-					<?php if (!empty($content)) : ?>
-						<div class="tertiary-cta__body">
-							<?php echo $content; ?>
 						</div>
 					<?php endif; ?>
 
-					<?php
-						get_acf_components([
-							'buttons' => [
-								'data'    => $buttons_group,
-								'classes' => 'tertiary-cta__buttons',
-							],
-						]);
-					?>
+					<?php if (!empty($content)) : ?>
+						<p class="tertiary-cta__body">
+							<?php echo $content; ?>
+						</p>
+					<?php endif; ?>
 				</div>
 
-				<?php if ($media_type === 'image' && !empty($image)) : ?>
-					<div class="tertiary-cta__media">
+					<div class="tertiary-cta__buttons">
 						<?php
-							if (!empty($image['id'])) {
-								if (function_exists('bis_get_attachment_picture')) {
-									echo bis_get_attachment_picture(
-										$image['id'],
-										[
-											560  => [ 300, 300, 1 ],
-											1024 => [ 300, 300, 1 ],
-											2800 => [ 300 * 2, 300 * 2, 1 ],
-										],
-										[
-											'alt'     => $image['alt'] ? $image['alt'] : wp_strip_all_tags($heading ?? ''),
-											'class'   => 'tertiary-cta__image',
-											'loading' => 'lazy',
-										]
-									);
-								} else {
-									$img_src = wp_get_attachment_image_url($image['id'], 'large');
-									$img_alt = get_post_meta($image['id'], '_wp_attachment_image_alt', true) ?: wp_strip_all_tags($heading ?? '');
-									echo '<img class="tertiary-cta__image" src="' . esc_url($img_src) . '" alt="' . esc_attr($img_alt) . '" loading="lazy" />';
-								}
-							} elseif (!empty($image['url'])) {
-								echo '<img class="tertiary-cta__image" src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt'] ?? '') . '" loading="lazy" />';
-							}
+							get_acf_component(
+								'action-group', [
+									'data' => $buttons,
+									// 'classes' => 'main-cta__btns',
+								],
+							);
 						?>
 					</div>
-				<?php endif; ?>
 			</div>
 		</div>
 	</section>
