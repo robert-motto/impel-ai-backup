ab59a0334f394837628d056e2752fd482b237914
Robert Krawczyk <robert@wearemotto.com>
2025-05-13 13:46:23 +0200

    feat: Hero

--- parts/blocks/hero/template.php
+++ parts/blocks/hero/template.php
@@ -7,58 +7,38 @@
 	<?php block_preview(__FILE__); ?>
 <?php else :?>
 	<?php
-		$group       = blockFieldGroup(__FILE__);
-		$heading     = $group['heading'] ?? '';
-		$subheading  = $group['subheading'] ?? '';
-		$buttons     = $group['buttons_group'] ?? [];
-		$media_type  = $group['media_type'] ?? 'image'; // Default to image
-		$image       = $group['image'] ?? null;
-		$video_group = $group['video_group'] ?? []; // Get video data if type is video
-		// Set fallback image if none is selected and media type is image
-		if ($media_type === 'image' && (empty($image) || !isset($image['id']))) {
-			$fallback_image_path = get_template_directory() . '/screenshot.jpg';
-			$fallback_image_uri = get_template_directory_uri() . '/screenshot.jpg';
-			if (file_exists($fallback_image_path)) {
-				$fallback_attachment = attachment_url_to_postid($fallback_image_uri);
-				if ($fallback_attachment) {
-					$image = ['id' => $fallback_attachment];
-				} else {
-					$image = [
-						'url' => $fallback_image_uri,
-						'alt' => wp_strip_all_tags($heading ?? 'Hero image')
-					];
-				}
-			}
-		}
+		$group       				= blockFieldGroup(__FILE__);
+		$color_mode      		= $group['section_settings_group']['mode'] ?? 'light';
+		$color_mode_variant	= $group['mode_variant'] ?? 'regular';
+		$heading         		= $group['heading_box_group'] ?? '';
+		$buttons 						= $group['action_group_group'] ?? [];
+		$media_type  				= $group['media_type'] ?? 'image';
+		$media_position  		= $group['media_position'] ?? 'image';
+		$text_position  		= $group['text_position'] ?? 'image';
+		$image       				= $group['image'] ?? null;
+		$video_group 				= $group['video_group'] ?? [];
+
 		// Section classes
-		$classes = ['l-section', 'l-section--hero'];
-		if (!empty($group['section_settings_group']['background_color'])) {
-			$classes[] = 'is-' . $group['section_settings_group']['background_color'];
-		} else {
-			$classes[] = 'is-light';
-		}
+		$classes = ['l-section', 'l-section--hero', "color-mode-{$color_mode}", "color-mode-variant-{$color_mode_variant}", "media-position-{$media_position}", "text-position-{$text_position}"];
 	?>
 	<section class="<?php echo esc_attr(implode(' ', $classes)); ?> <?php echo section_settings_padding_classes($group);?>" data-block="hero">
 		<div class="l-wrapper">
 			<div class="hero">
 				<div class="hero__text-hld">
-					<?php if (!empty($heading)) : ?>
-						<div class="hero__heading ">
-							<?php echo $heading; ?>
-						</div>
-					<?php endif; ?>
-					<?php if (!empty($subheading)) : ?>
-						<div class="hero__subheading">
-							<?php echo $subheading; ?>
-						</div>
-					<?php endif; ?>
 					<?php
-						get_acf_components([
-							'buttons' => [
-								'data'    => $buttons,
+						get_acf_component(
+							'heading-box', [
+								'data' => $heading,
+							],
+						);
+					?>
+					<?php
+						get_acf_component(
+							'action-group', [
+								'data' => $buttons,
 								'classes' => 'hero__btns',
 							],
-						]);
+						);
 					?>
 				</div>
 				<div class="hero__media-hld">
@@ -68,8 +48,8 @@
 								echo bis_get_attachment_picture(
 									$image['id'],
 									[
-										560  => [ 390 * 2, 772 * 2, 1 ],
-										1024 => [ 1024, 1474, 1 ],
+										560  => [ 390 * 2, 210 * 2, 1 ],
+										1024 => [ 1024, 553, 1 ],
 										1365 => [ 1400, 760, 1 ],
 										2800 => [ 1400 * 2, 760 * 2, 1 ],
 									],
