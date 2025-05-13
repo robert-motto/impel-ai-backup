6bbf75d78ed366c94de8e52fe93a407e6a03960c
Robert Krawczyk <robert@wearemotto.com>
2025-05-13 15:00:19 +0200

    feat: Logos Certification

--- parts/blocks/logos-certification/logos-certification.js
+++ parts/blocks/logos-certification/logos-certification.js
@@ -1,8 +1,7 @@
 import Swiper from 'swiper';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 
 function initLogosCarousels() {
@@ -13,19 +12,14 @@ function initLogosCarousels() {
 	}
 
 	carousels.forEach((carouselElement, index) => {
-		const displayMode = carouselElement.dataset.displayMode;
 		const autoplaySpeed = parseInt(carouselElement.dataset.autoplay, 10) || 3000;
 		const pauseOnHover = carouselElement.dataset.pauseHover === 'true';
-		const slidesPerView = parseInt(carouselElement.dataset.slidesPerView, 10) || 5;
-
-		const prevButton = carouselElement.querySelector('.js-logos-prev');
-		const nextButton = carouselElement.querySelector('.js-logos-next');
 
 		const slides = carouselElement.querySelectorAll('.swiper-slide');
 
 		try {
 			const swiperConfig = {
-				modules: displayMode === 'carousel' ? [Navigation, Autoplay] : [],
+				modules: [Autoplay],
 				slidesPerView: 2,
 				spaceBetween: 16,
 				loop: true,
@@ -52,34 +46,20 @@ function initLogosCarousels() {
 						spaceBetween: 30,
 					},
 					1280: {
-						slidesPerView: slidesPerView,
-						spaceBetween: 40,
+						spaceBetween: 88,
 					},
 				},
 			};
 
-			if (displayMode === 'carousel') {
-				swiperConfig.autoplay = {
-					delay: autoplaySpeed,
-					disableOnInteraction: pauseOnHover,
-					pauseOnMouseEnter: pauseOnHover,
-				};
-
-				swiperConfig.navigation = {
-					nextEl: nextButton,
-					prevEl: prevButton,
-				};
-			} else {
-				swiperConfig.freeMode = {
-					enabled: true,
-					sticky: false,
-				};
-				swiperConfig.allowTouchMove = true;
-			}
+			swiperConfig.autoplay = {
+				delay: autoplaySpeed,
+				disableOnInteraction: pauseOnHover,
+				pauseOnMouseEnter: pauseOnHover,
+			};
 
 			const swiper = new Swiper(carouselElement, swiperConfig);
 
-			if (displayMode === 'carousel' && pauseOnHover) {
+			if (pauseOnHover) {
 				carouselElement.addEventListener('mouseenter', () => {
 					swiper.autoplay.stop();
 				});
