e2f345adfed5755098b5cfdc1079eafe621df85f
Robert Krawczyk <robert@wearemotto.com>
2025-05-15 10:53:37 +0200

    feat: Bento Grid

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -17,7 +17,7 @@
 				'acf/featured-posts',
 				'acf/hero',
 				'acf/hero-left',
-			'acf/hero-secondary',
+				'acf/hero-secondary',
 				'acf/content-block-left-right',
 				'acf/content-block-left-right-with-metrics',
 				'acf/before-after-comparison',
@@ -38,7 +38,8 @@
 				'acf/shortlist',
 				'acf/related-content',
 				'acf/general-form',
-			'acf/vertical-scrolling-cards',
+				'acf/vertical-scrolling-cards',
+				'acf/bento-grid',
 			);
 		} else {
 			return array(
