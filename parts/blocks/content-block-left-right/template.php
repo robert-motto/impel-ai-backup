551b9b346687fdd38da8cb4a404cf7de8c95ff1b
Robert <robert@adchitects.co>
2025-05-30 12:07:38 +0200

    feat: Content Block Left Right - add metric box

--- parts/blocks/content-block-left-right/template.php
+++ parts/blocks/content-block-left-right/template.php
@@ -18,6 +18,9 @@
 	$image           		= $group['image'] ?? '';
 	$video_group     		= $group['video_group'] ?? [];
 	$image_display_size = $group['image_display_size'] ?? 'regular';
+	$show_metric        = $group['show_metric'] ?? 0;
+	$metric_icon        = $group['metric_icon'] ?? 'arrow-up';
+	$metric_text        = $group['metric_text'] ?? '';
 
 	// Set fallback image if none is selected and media type is image
 	if ($media_type === 'image' && (empty($image) || !isset($image['id']))) {
@@ -86,9 +89,9 @@
 							$image['id'],
 							[
 								560  => [560, 560, 1],
-								1024 => [720, 720, 1],
-								1920 => [720, 720, 1],
-								2800 => [720 * 2, 720 * 2, 1],
+								1024 => [564, 634, 1],
+								1920 => [564, 634, 1],
+								2800 => [564 * 2, 634 * 2, 1],
 							],
 							[
 								'alt'     => $image['alt'] ? $image['alt'] : wp_strip_all_tags((is_array($heading) && isset($heading['heading']) && is_string($heading['heading'])) ? $heading['heading'] : ''),
@@ -109,6 +112,25 @@
 						]
 					]);
 					?>
+				<?php endif; ?>
+
+				<?php if ($show_metric) : ?>
+					<div class="content-block__metric">
+						<div class="content-block__metric-icon">
+							<?php
+							$icon_name = 'check';
+							if ($metric_icon === 'arrow-up') {
+								$icon_name = 'arrow-up-right';
+							} elseif ($metric_icon === 'arrow-down') {
+								$icon_name = 'arrow-down-right';
+							}
+							get_icon($icon_name);
+							?>
+						</div>
+						<div class="content-block__metric-text">
+							<?php echo esc_html($metric_text); ?>
+						</div>
+					</div>
 				<?php endif; ?>
 			</div>
 		</div>
