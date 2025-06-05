48d45e88576716ecb93204428024d4b0a484cfe6
Marcin <marcin@wearemotto.com>
2025-06-05 03:14:20 +0200

    chore: build

--- tmp/parts/blocks/single-media-section/fields.php
+++ tmp/parts/blocks/single-media-section/fields.php
@@ -16,43 +16,9 @@
 					'group_section_settings',
 				]
 			])
-			->addButtonGroup('alignment', [
-				'label' => 'Content Alignment',
-				'choices' => [
-					'center' => 'Center',
-					'left' => 'Left',
-				],
-				'default_value' => 'center',
-				'layout' => 'horizontal',
-				'return_format' => 'value',
-			])
-			->addText('caption', [
-				'label' => 'Caption',
-				'instructions' => 'Short text above the heading',
-				'default_value' => 'Tagline',
-			])
-			->addWysiwyg('heading', [
-				'label' => 'Heading',
-				'instructions' => 'Enter the main heading text. Use Enter/Return for line breaks.',
-				'media_upload' => 0,
-				'toolbar' => 'basic',
-				'tabs' => 'visual',
-				'delay' => 0,
-				'new_lines' => 'br',
-				'default_value' => 'Short heading goes here',
-			])
-			->addWysiwyg('content', [
-				'label' => 'Body Content',
-				'instructions' => 'Enter the main content text. Use the toolbar for lists and formatting.',
-				'media_upload' => 0,
-				'toolbar' => 'full',
-				'tabs' => 'all',
-				'delay' => 0,
-				'default_value' => '<p>Ornare purus enim pulvinar at volutpat. Arcu lobortis elementum eu consectetur. Diam proin et senectus condimentum imperdiet vitae nisi. Velit habitasse odio libero.</p>',
-			])
-			->addField('buttons', 'clone', [
+			->addField('heading', 'clone', [
 				'clone' => [
-					'group_buttons',
+					'group_heading_box',
 				]
 			])
 			->addRadio('media_type', [
@@ -78,7 +44,6 @@
 					'group_video',
 				],
 				'display' => 'seamless',
-				'prefix_name' => true,
 			])
 				->conditional('media_type', '==', 'video')
 			->addText('media_caption', [
@@ -86,26 +51,6 @@
 				'instructions' => 'Add an optional caption for the media (displayed at the top right corner of the image or video)',
 				'default_value' => '',
 			])
-			->addTrueFalse('has_background', [
-				'label' => 'Use Background Color',
-				'instructions' => 'Add background color to the section',
-				'default_value' => 0,
-				'ui' => 1,
-			])
-			->addSelect('background_color', [
-				'label' => 'Background Color',
-				'instructions' => 'Select background color',
-				'choices' => [
-					'light' => 'Light (Gray)',
-					'white' => 'White',
-					'dark' => 'Dark',
-				],
-				'default_value' => 'light',
-				'return_format' => 'value',
-				'multiple' => 0,
-				'ui' => 1,
-			])
-				->conditional('has_background', '==', 1)
 		->endGroup()
 		->setLocation('block', '==', 'acf/' . $path);
 	return $name;
