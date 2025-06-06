0077e51e663e24046d1676c2a7035cd4797da990
Robert <robert@adchitects.co>
2025-06-06 13:42:31 +0200

    feat: Add linear option to slider

--- parts/components/slider.php
+++ parts/components/slider.php
@@ -11,6 +11,7 @@
 	$space_between = $slider_settings['space_between'] ?? '';
 	$loop = $slider_settings['loop'] ?? '';
 	$slides_offset_before = $slider_settings['slides_offset_before'] ?? '';
+	$linear = $slider_settings['linear'] ?? false;
 ?>
 
 <?php if (!empty($slides)) : ?>
@@ -23,7 +24,8 @@
       data-show-progressbar="<?php echo esc_attr($show_progressbar ? 'true' : 'false'); ?>"
       data-space-between="<?php echo esc_attr($space_between); ?>"
       data-loop="<?php echo esc_attr($loop ? 'true' : 'false'); ?>"
-	  data-slides-offset-before="<?php echo esc_attr($slides_offset_before); ?>">
+	  data-slides-offset-before="<?php echo esc_attr($slides_offset_before); ?>"
+	  data-linear="<?php echo esc_attr($linear ? 'true' : 'false'); ?>">
       <div class="swiper-wrapper">
 
         <?php foreach ($slides as $index => $slide) : ?>
